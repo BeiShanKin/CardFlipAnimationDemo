163c332e64e9e1437df7fd3c9a97c04364a10edf
songyu <syiosengineer@163.com>
2015-09-11 10:32:29 +0800

    Initial Commit

---
+++ CALayerDemo3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CALayerDemo3
+//
+//  Created by lz on 15/9/11.
+//  Copyright (c) 2015年 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CALayerDemo3Tests/CALayerDemo3Tests.m
@@ -0,0 +1,40 @@
+//
+//  CALayerDemo3Tests.m
+//  CALayerDemo3Tests
+//
+//  Created by lz on 15/9/11.
+//  Copyright (c) 2015年 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CALayerDemo3Tests : XCTestCase
+
+@end
+
+@implementation CALayerDemo3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
